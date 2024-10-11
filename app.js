585212d3d54850b8819761fb434b76ad9120d2a0
tsune <tsune@aluco.jp>
2024-10-11 11:32:04 +0900

    [Fix] タスクの完了、戻すボタンの機能追加、完成

--- app.js
+++ app.js
@@ -2,19 +2,36 @@ const onClickAdd = () => {
   const inputText = document.getElementById('add-text').value;
   document.getElementById('add-text').value = "";
 
+  createIncompleteTodo(inputText);
+}
+
+const createIncompleteTodo = (todo) => {
   const ul = document.getElementById('incomplete-list');
   const li = document.createElement('li');
   const div = document.createElement('div');
   div.className = 'list-row';
   const p = document.createElement('p');
   p.className = 'todo-item';
-  p.innerText = inputText;
+  p.innerText = todo;
 
   const completeButton = document.createElement('button');
   completeButton.innerText = '完了';
 
   completeButton.addEventListener('click', () => {
-    alert();
+    const moveTarget = completeButton.closest('li');
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    const backButton = document.createElement('button');
+    backButton.innerText = '戻す';
+    backButton.addEventListener('click', () => {
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+      moveTarget.remove();
+    })
+
+
+    moveTarget.firstElementChild.appendChild(backButton);
+    document.getElementById('complete-list').appendChild(moveTarget);
   });
 
   const deleteButton = document.createElement('button');
